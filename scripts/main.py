c9a59b9141da5fb99d496c08e7f22033420b3dba
pc_win10 <pc_win10@me.com>
2021-02-22 17:59:50 +0800

    mark extract ok

--- scripts/main.py
+++ scripts/main.py
@@ -23,23 +23,25 @@ def judgeModifyTime(path):
 
 # 3. get mark list and pend to file
 def pendMDMark(path):
+    orign = []
     marks = []
     marks_flag = False
-    with open(path, "r+") as f:
-        line = f.readline()
-        while line:
-            if re.match("[Qu：]|[Cm：]", line):
-                marks.append(line.strip())
+    with open(path, "r") as f_r:
+        lines = f_r.readlines()
+        
+    with open(path, "w") as f_w:
+        for line in lines:
+            f_w.write(line)
+            if re.match("(Qu：)|(Cm：)|(Td：)", line):
+                marks.append("- " + line.strip() + "\n")
+                continue
             if re.match("## Marks*", line):
                 marks_flag = True
                 break
-            line = f.readline()
-            print(line)
+        
         if marks_flag == False:
-            f.write("## Mark\n")
-        print(marks_flag)
-        for each in marks:
-            f.write(each)
+            f_w.write("\n## Mark\n")
+        f_w.writelines(marks)
 
 
 
